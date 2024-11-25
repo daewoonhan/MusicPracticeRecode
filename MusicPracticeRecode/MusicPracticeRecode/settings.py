e08f38986794f4f078a198303019c85c7d1742c4
daewoonhan <gkseodns1106@gmail.com>
2024-11-25 14:46:40 +0900

    앱 생성 및 세팅

--- MusicPracticeRecode/MusicPracticeRecode/settings.py
+++ MusicPracticeRecode/MusicPracticeRecode/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'user.apps.UserConfig',
+    'search.apps.SearchConfig',
+    'mypage.apps.MypageConfig'
 ]
 
 MIDDLEWARE = [
